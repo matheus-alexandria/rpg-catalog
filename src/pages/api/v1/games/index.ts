856f204ae0032c6e55277d7868434775eacdfc4a
Matchobas <maalexandria@hotmail.com>
2024-05-10 20:52:26 -0300

    feat: create first get route

--- src/pages/api/v1/games/index.ts
+++ src/pages/api/v1/games/index.ts
@@ -1,5 +1,11 @@
+import { prisma } from "@/utils/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function games(request: NextApiRequest, response: NextApiResponse) {
-  return response.status(200).json({ message: 'respondendo'});
+export default async function games(request: NextApiRequest, response: NextApiResponse) {
+  if (request.method === 'GET') {
+    const games = await prisma.game.findMany();
+
+    return response.status(200).json(games);
+  }
+  return response.status(400).json({ message: 'Nenhum método encontrado'});
 }
