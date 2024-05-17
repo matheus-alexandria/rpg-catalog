43af7adf5a34fa70e8160bcc365309ae708f8ca0
Matchobas <maalexandria@hotmail.com>
2024-05-16 21:09:51 -0300

    feat: adding more icons and exporting them all at once

---
+++ public/icons/index.ts
@@ -0,0 +1,9 @@
+import d20Icon from "./dice-twenty-faces-twenty.svg";
+import bookCover from "./book-cover.svg";
+import trailPath from "./trail.svg";
+
+export const icons = {
+  d20: d20Icon,
+  bookCover: bookCover,
+  trailPath: trailPath
+}
