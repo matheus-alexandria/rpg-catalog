9f5990c714a00940a389d431a215388fe5155b2d
Matchobas <maalexandria@hotmail.com>
2024-05-24 23:35:47 -0300

    feat: starting form logic on add game modal

--- src/components/AddGameModal.tsx
+++ src/components/AddGameModal.tsx
@@ -1,11 +1,26 @@
+'use client'
+
+import { FormEvent, useState } from "react";
+
 export default function AddGameModal() {
+  const [game, setGame] = useState('');
+
+  function sendFormData(event: FormEvent) {
+    event.preventDefault();
+    console.log(game);
+  }
+
   return (
     <div className="absolute h-80 w-96 left-1/2 top-1/2 bg-red-400">
       <h3>Adicionar novo jogo</h3>
-      <form>
+      <form onSubmit={(e) => sendFormData(e)}>
         <p>Insira o nome do RPG de mesa:</p>
-        <input type="text" placeholder="Dungeons & Dragons"></input>
-        <button>Criar</button>
+        <input 
+          type="text"
+          placeholder="Dungeons & Dragons"
+          onChange={(e) => setGame(e.target.value)}
+        ></input>
+        <button type="submit">Criar</button>
       </form>
     </div>
   )
