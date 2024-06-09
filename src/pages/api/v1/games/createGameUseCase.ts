dc929ab0e67baf03cffda7dbc12321a76b2a1c38
Matchobas <maalexandria@hotmail.com>
2024-06-09 20:26:33 -0300

    refactor: also create themes links when creating a game

--- src/pages/api/v1/games/createGameUseCase.ts
+++ src/pages/api/v1/games/createGameUseCase.ts
@@ -1,15 +1,35 @@
+import { NotFoundError } from '@/pages/errors';
 import { prisma } from '@/utils/prisma';
 
 export class CreateGameUseCase {
   async execute(params: CreateGameParams) {
-    const { title, description, dice, theme, gameplay_focus } = params;
+    const { title, description, dice, themes, gameplay_focus } = params;
+
+    const foundThemes = await prisma.theme.findMany({
+      where: {
+        name: {
+          in: themes
+        }
+      }
+    });
+
+    if (!foundThemes.length) {
+      throw new NotFoundError('No themes found with the given names.');
+    }
 
     const game = await prisma.game.create({
       data: {
         title,
         description,
         dice,
-        theme,
+        GameTheme: {
+          createMany: {
+            data: foundThemes.map((theme) => ({
+              theme_id: theme.id,
+              is_main: false
+            }))
+          }
+        },
         gameplay_focus
       }
     });
@@ -22,6 +42,6 @@ interface CreateGameParams {
   title: string;
   description: string;
   dice: string;
-  theme: string;
+  themes: string[];
   gameplay_focus: string;
 }
