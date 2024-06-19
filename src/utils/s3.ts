dabaf38e4a4a9be5ae859e84c7c9f6e5b032cf04
Matchobas <maalexandria@hotmail.com>
2024-06-18 21:34:13 -0300

    chore: starting to implement aws s3 bucket

---
+++ src/utils/s3.ts
@@ -0,0 +1,18 @@
+import { S3 } from 'aws-sdk';
+
+const s3 = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+export const uploadFile = (fileContent: any, bucketName: string, key: string) => {
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+    Body: fileContent,
+    ACL: 'public-read'
+  };
+
+  return s3.upload(params).promise();
+};
