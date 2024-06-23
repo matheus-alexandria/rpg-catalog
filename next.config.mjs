1ce71f5e5f2e3a06734f0b92280239b687ee8895
Matchobas <maalexandria@hotmail.com>
2024-06-22 22:14:02 -0300

    refactor: add AWS as possible image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rpg-catalog-bucket.s3.amazonaws.com',
+        port: '',
+        pathname: '/upload/**'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
