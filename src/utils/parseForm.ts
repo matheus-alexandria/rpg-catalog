16c1b060373f3c596ee874cebf64b596245e1d81
Matchobas <maalexandria@hotmail.com>
2024-06-23 23:36:57 -0300

    chore: parse the fields receive in form data

--- src/utils/parseForm.ts
+++ src/utils/parseForm.ts
@@ -1,18 +1,31 @@
 import formidable, { Fields, Files } from 'formidable';
 import { NextApiRequest } from 'next';
 
-export async function parseForm(req: NextApiRequest): Promise<{ fields: Fields; files: Files }> {
+export async function parseForm(
+  req: NextApiRequest
+): Promise<{ parsedFields: Record<string, any>; files: Files }> {
   return new Promise((resolve, rejects) => {
     const form = formidable({
-      maxFiles: 10,
-      maxFileSize: 1024 * 1024 * 10
+      maxFiles: 1,
+      maxFileSize: 1024 * 1024 * 2
     });
 
     form.parse(req, (err: any, fields: Fields, files: Files) => {
+      const parsedFields = parseFields(fields);
       if (err) {
         return rejects(err.message);
       }
-      resolve({ fields, files });
+      resolve({ parsedFields, files });
     });
   });
+}
+
+function parseFields(fields: Fields): Record<string, any> {
+  const parsed: Record<string, any> = {};
+  for (const [key, value] of Object.entries(fields)) {
+    if (value) {
+      parsed[key] = value[0];
+    }
+  }
+  return parsed;
 }
