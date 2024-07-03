39966c2e0f498bf4375ce417c21084127dc7b1ea
Matchobas <maalexandria@hotmail.com>
2024-07-02 21:46:47 -0300

    refactor: create a specific route to create a new game

---
+++ src/pages/api/v1/games/create/index.ts
@@ -0,0 +1,52 @@
+import { NotFoundError } from '@/errors';
+import { parseForm } from '@/utils/parseForm';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ZodError, z } from 'zod';
+import { CreateGameUseCase } from '../createGameUseCase';
+
+export default async function create(request: NextApiRequest, response: NextApiResponse) {
+  if (request.method === 'POST') {
+    try {
+      const { parsedFields: fields, files } = await parseForm(request);
+      const createGameSchema = z.object({
+        title: z.string().min(1),
+        description: z.string().min(1),
+        dice: z.string().min(1),
+        themes: z.array(z.string()).min(1),
+        gameplay_focus: z.string().min(1)
+      });
+      const { title, description, dice, themes, gameplay_focus } = createGameSchema.parse(fields);
+      console.log(themes);
+
+      const createGameUseCase = new CreateGameUseCase();
+      const game = await createGameUseCase.execute({
+        title,
+        description,
+        dice,
+        themes,
+        gameplay_focus
+      });
+
+      return response.status(200).json(game);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        return response.status(422).json({
+          message: 'Incomplete or invalid data',
+          error: err.errors
+        });
+      }
+      if (err instanceof NotFoundError) {
+        return response.status(404).json({
+          message: err.message
+        });
+      }
+      throw err;
+    }
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
