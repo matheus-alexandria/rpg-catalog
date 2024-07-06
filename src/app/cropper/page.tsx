b3ffb9591b73342902b340260c1fe496f838c15e
Matchobas <maalexandria@hotmail.com>
2024-07-05 21:27:41 -0300

    chore: adapting the cropper tool

--- src/app/cropper/page.tsx
+++ src/app/cropper/page.tsx
@@ -5,22 +5,27 @@ import 'cropperjs/dist/cropper.css';
 
 export default function Crop() {
   const cropperRef = useRef<ReactCropperElement>(null);
+  const croppedUrl = useRef<string>('');
   const onCrop = () => {
     const cropper = cropperRef.current?.cropper;
     if (cropper) {
-      console.log(cropper.getCroppedCanvas().toDataURL());
+      croppedUrl.current = cropper.getCroppedCanvas().toDataURL();
     }
   };
 
   return (
-    <Cropper
-      src="https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg"
-      style={{ height: 400, width: '100%' }}
-      // Cropper.js options
-      initialAspectRatio={3 / 4}
-      guides={false}
-      crop={onCrop}
-      ref={cropperRef}
-    />
+    <>
+      <Cropper
+        src="https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg"
+        style={{ height: 400, width: '100%' }}
+        // Cropper.js options
+        initialAspectRatio={3 / 4}
+        aspectRatio={3 / 4}
+        guides={false}
+        crop={onCrop}
+        ref={cropperRef}
+      />
+      <button type="button">Send</button>
+    </>
   );
 }
