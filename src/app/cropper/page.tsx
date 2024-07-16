843ad901882321a2fc0d22627a16b4e9f7f50698
Matchobas <maalexandria@hotmail.com>
2024-07-15 21:58:08 -0300

    chore: create button to upload

--- src/app/cropper/page.tsx
+++ src/app/cropper/page.tsx
@@ -7,16 +7,9 @@ import 'cropperjs/dist/cropper.css';
 export default function Crop() {
   const cropperRef = useRef<ReactCropperElement>(null);
   const croppedUrl = useRef<string>('');
-  const [file, setFile] = useState<File>();
+  const [file, setFile] = useState<File | null>(null);
   const [imagePath, setImagePath] = useState<string | null>(null);
-  const onCrop = () => {
-    const cropper = cropperRef.current?.cropper;
-    if (cropper) {
-      cropper.getCroppedCanvas().toBlob((blob) => {
-        console.log(blob);
-      });
-    }
-  };
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length > 0) {
       const file = acceptedFiles[0];
@@ -33,22 +26,44 @@ export default function Crop() {
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
+  const uploadImage = useCallback(() => {
+    const cropper = cropperRef.current?.cropper;
+    if (cropper && file) {
+      const form = new FormData();
+      form.append('file', file);
+      cropper.getCroppedCanvas().toBlob((blob) => {
+        fetch('/api/v1/games/upload', {
+          method: 'POST',
+          body: form
+        });
+      });
+    }
+  }, [file]);
+
   return (
     <>
       <div className="w-full h-screen bg-coolGray-900 flex justify-center items-center">
         {imagePath && (
-          <Cropper
-            src={imagePath}
-            style={{ height: 400 }}
-            initialAspectRatio={3 / 4}
-            aspectRatio={3 / 4}
-            guides={false}
-            viewMode={2}
-            background={false}
-            modal={false}
-            crop={onCrop}
-            ref={cropperRef}
-          />
+          <div>
+            <Cropper
+              src={imagePath}
+              style={{ height: 400 }}
+              initialAspectRatio={3 / 4}
+              aspectRatio={3 / 4}
+              guides={false}
+              viewMode={2}
+              background={false}
+              modal={false}
+              ref={cropperRef}
+            />
+            <button
+              type="button"
+              className="bg-orange-400 text-white font-bold p-5"
+              onClick={() => uploadImage()}
+            >
+              Upload
+            </button>
+          </div>
         )}
         <div {...getRootProps()}>
           <input {...getInputProps()} />
