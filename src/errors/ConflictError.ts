1b34e99fcb8ba8240c41b0000ece3428fc79d006
Matchobas <maalexandria@hotmail.com>
2024-07-16 21:13:04 -0300

    chore: create bad request error and change message

--- src/errors/ConflictError.ts
+++ src/errors/ConflictError.ts
@@ -1,7 +1,7 @@
 export class ConflictError extends Error {
   status = 409;
 
-  constructor(message = 'This action conflict with the state of the server.') {
+  constructor(message = 'This action conflicts with the state of the server.') {
     super(message);
   }
 }
