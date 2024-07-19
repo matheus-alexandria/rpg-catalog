b8aad3587f17d8b1ef37685dce3474a2ab12e4eb
Matchobas <maalexandria@hotmail.com>
2024-07-18 22:50:18 -0300

    chore: adapt create route to save cover file

--- src/pages/api/v1/games/createGameUseCase.ts
+++ src/pages/api/v1/games/createGameUseCase.ts
@@ -1,9 +1,13 @@
+import { readFile } from 'node:fs/promises';
 import { NotFoundError } from '@/errors';
+import { BadRequestError } from '@/errors/BadRequestError';
 import { prisma } from '@/utils/prisma';
+import { uploadFile } from '@/utils/s3';
+import { File } from 'formidable';
 
 export class CreateGameUseCase {
   async execute(params: CreateGameParams) {
-    const { title, description, dice, themes, gameplay_focus } = params;
+    const { title, description, dice, themes, gameplay_focus, file } = params;
 
     const foundThemes = await prisma.theme.findMany({
       where: {
@@ -17,11 +21,29 @@ export class CreateGameUseCase {
       throw new NotFoundError('No themes found with the given names.');
     }
 
+    let pathToS3File: string | null = null;
+    if (file) {
+      const path = file?.filepath;
+      if (path) {
+        const fileContent = await readFile(path);
+        const key = `upload/${file.originalFilename}-${new Date()}`;
+
+        try {
+          const bucketName = process.env.S3_BUCKET_NAME;
+          const result = await uploadFile(fileContent, bucketName ?? '', key);
+          pathToS3File = result.Location;
+        } catch (e: any) {
+          throw new BadRequestError(`Erros while uploading the file: ${e?.message}`);
+        }
+      }
+    }
+
     const game = await prisma.game.create({
       data: {
         title,
         description,
         dice,
+        cover_path: pathToS3File,
         game_themes: {
           createMany: {
             data: foundThemes.map((theme) => ({
@@ -51,4 +73,5 @@ interface CreateGameParams {
   dice: string;
   themes: string[];
   gameplay_focus: string;
+  file?: File;
 }
