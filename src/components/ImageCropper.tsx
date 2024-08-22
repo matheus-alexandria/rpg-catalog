7faa00db12f4905ac60c5517a92c2822c402a669
Matchobas <maalexandria@hotmail.com>
2024-08-21 21:54:40 -0300

    fix: trying to fix style of cropper component

--- src/components/ImageCropper.tsx
+++ src/components/ImageCropper.tsx
@@ -1,51 +1,51 @@
 'use client';
 
-import { useCallback, useRef } from 'react';
+import { Dispatch, SetStateAction, useCallback, useRef } from 'react';
 import { Cropper, ReactCropperElement } from 'react-cropper';
 
 interface ImageCropperProps {
-  file: File | null;
   imagePath: string;
+  addGameForm: FormData;
+  setImagePath: Dispatch<SetStateAction<string | null>>;
 }
 
-export default function ImageCropper({ file, imagePath }: ImageCropperProps) {
+export default function ImageCropper({ imagePath, addGameForm, setImagePath }: ImageCropperProps) {
   const cropperRef = useRef<ReactCropperElement>(null);
   const croppedUrl = useRef<string>('');
 
   const uploadImage = useCallback(() => {
     const cropper = cropperRef.current?.cropper;
-    if (cropper && file) {
-      const form = new FormData();
-      form.append('file', file);
+    if (cropper) {
       cropper.getCroppedCanvas().toBlob((blob) => {
-        fetch('/api/v1/games/upload', {
-          method: 'POST',
-          body: form
-        });
+        if (blob) addGameForm.append('file', blob);
+        setImagePath(null);
       });
     }
-  }, [file]);
+  }, [addGameForm, setImagePath]);
 
   return (
-    <div>
-      <Cropper
-        src={imagePath}
-        style={{ height: 400 }}
-        initialAspectRatio={3 / 4}
-        aspectRatio={3 / 4}
-        guides={false}
-        viewMode={2}
-        background={false}
-        modal={false}
-        ref={cropperRef}
-      />
-      <button
-        type="button"
-        className="bg-orange-400 text-white font-bold p-5"
-        onClick={() => uploadImage()}
-      >
-        Recortar
-      </button>
-    </div>
+    <>
+      {/* <div className="fixed h-screen w-screen top-0 left-0 z-5 bg-black opacity-20" /> */}
+      <div className="fixed z-50">
+        <Cropper
+          src={imagePath}
+          style={{ height: 400 }}
+          initialAspectRatio={3 / 4}
+          aspectRatio={3 / 4}
+          guides={false}
+          viewMode={2}
+          background={true}
+          modal={true}
+          ref={cropperRef}
+        />
+        <button
+          type="button"
+          className="bg-orange-400 text-white font-bold p-5 hover:bg-orange-500 transition-colors"
+          onClick={() => uploadImage()}
+        >
+          Recortar
+        </button>
+      </div>
+    </>
   );
 }
