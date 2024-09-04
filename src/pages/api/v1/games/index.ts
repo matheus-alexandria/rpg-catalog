6b5da293c31538d719724fd40c6f0a59e606477f
Matchobas <maalexandria@hotmail.com>
2024-09-03 22:19:52 -0300

    chore: creating route controller for updateGameUseCase

--- src/pages/api/v1/games/index.ts
+++ src/pages/api/v1/games/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { ZodError, z } from 'zod';
 import { DeleteGameUseCase } from './deleteGameUseCase';
 import { GetAllGamesUseCase } from './getAllGamesUseCase';
+import { UpdateGameUseCase } from './updateGameUseCase';
 
 export default async function games(request: NextApiRequest, response: NextApiResponse) {
   if (request.method === 'GET') {
@@ -9,6 +10,41 @@ export default async function games(request: NextApiRequest, response: NextApiRe
     const games = await getAllGamesUseCase.execute();
 
     return response.status(200).json(games);
+  }
+
+  if (request.method === 'PUT') {
+    try {
+      const updateGameSchema = z.object({
+        title: z.string().min(1).optional(),
+        description: z.string().min(1).optional(),
+        dice: z.string().min(1).optional(),
+        themes: z.array(z.string()).min(1).or(z.string().min(1)).optional(),
+        gameplay_focus: z.string().min(1).optional()
+      });
+      const { title, description, dice, themes, gameplay_focus } = updateGameSchema.parse(
+        request.body
+      );
+
+      const updateGameUseCase = new UpdateGameUseCase();
+      const game = await updateGameUseCase.execute({
+        title,
+        description,
+        dice,
+        themes,
+        gameplay_focus
+      });
+
+      return response.status(200).json(game);
+    } catch (err) {
+      if (err instanceof ZodError) {
+        return response.status(422).json({
+          message: 'Incomplete or invalid data',
+          error: err.errors
+        });
+      }
+
+      throw err;
+    }
   }
 
   if (request.method === 'DELETE') {
