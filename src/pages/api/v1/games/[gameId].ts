c012d8a77e85f92159f40aa9d750bf45bd81150e
Matchobas <maalexandria@hotmail.com>
2024-09-09 23:10:52 -0300

    feat: create and test get one route

--- src/pages/api/v1/games/[gameId].ts
+++ src/pages/api/v1/games/[gameId].ts
@@ -1,9 +1,28 @@
+import { NotFoundError } from '@/errors';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { ZodError, z } from 'zod';
+import { GetOneGamesUseCase } from './getOneGameUseCase';
 
 export default async function games(request: NextApiRequest, response: NextApiResponse) {
-  // TODO: getOne game route
-  const id = request.query.gameId;
-  return response.status(200).json({
-    id
-  });
+  try {
+    const querySchema = z.string().uuid();
+    const gameId = querySchema.parse(request.query.gameId);
+    const getOneGameUseCase = new GetOneGamesUseCase();
+    const game = await getOneGameUseCase.execute(gameId);
+    return response.status(200).json({
+      game
+    });
+  } catch (err) {
+    if (err instanceof NotFoundError) {
+      return response.status(err.status).json({
+        message: err.message
+      });
+    }
+    if (err instanceof ZodError) {
+      return response.status(422).json({
+        message: err.format()
+      });
+    }
+    throw err;
+  }
 }
